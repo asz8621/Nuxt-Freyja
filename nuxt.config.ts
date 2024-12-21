fbd33789227ca55c43b7c6454fcd447b5d384981
Nielsen <asz718460@gmail.com>
2024-12-21 15:44:25 +0800

    [新增] API 路徑

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['@/assets/styles/all.scss'],
+  runtimeConfig: {
+    public: {
+      API_URL: "",
+    }
+  },
 })
